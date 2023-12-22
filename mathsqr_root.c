3eff6507ab6d055f56c5c5dda59306cb1e1d10bf
Alokg <alokji812@gmail.com>
2023-12-22 14:25:01 +0530

    This is my first commit.

---
+++ mathsqr_root.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a;
+    printf("Enter the number whose sq root is req.: ");
+    scanf("%d",&a);
+    float root = sqrt(a);
+    printf("The square root of given number is %f",root);
+}
